90b4a4768df64975dec3c70fc3c234e6dfd13c71
102306028 <102306028@nccu.edu.tw>
2015-04-27 13:31:55 +0800

    finishgit add .

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427031047) do
+ActiveRecord::Schema.define(version: 20150427041017) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150427031047) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
